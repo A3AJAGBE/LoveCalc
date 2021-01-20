906a712212c3a61342efbc27879a31efd4318977
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-01-20 05:58:45 +0000

    Add user prompts

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+A program that tests the compatibility between two people.
+Using the super scientific method recommended by BuzzFeed.
+"""
+# Display a message to start
+print("Welcome to the A3AJAGBE Love Calculator!")
+
+# Prompt for the names
+your_name = input("What is your name? \n").lower()
+partner_name = input("What is their name? \n").lower()
+
+print(your_name)
+print(partner_name)
