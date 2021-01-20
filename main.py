e2783cfbd4fefbf81af9f536b59fae0e65de8d6d
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-01-20 06:14:00 +0000

    Add python statements

--- main.py
+++ main.py
@@ -9,5 +9,29 @@ print("Welcome to the A3AJAGBE Love Calculator!")
 your_name = input("What is your name? \n").lower()
 partner_name = input("What is their name? \n").lower()
 
-print(your_name)
-print(partner_name)
+# Combine the names
+combine = your_name + partner_name
+
+# Count the occurrence of 'true love' characters
+t = combine.count('t')
+r = combine.count('r')
+u = combine.count('u')
+e = combine.count('e')
+true = t + r + u + e
+l = combine.count('l')
+o = combine.count('o')
+v = combine.count('v')
+# e is missing because its already completed above
+love = l + o + v + e
+
+# Concatenate and convert to loop through
+truelove = str(true) + str(love)
+lovescore = int(truelove)
+
+# Interpret the output
+if (lovescore < 10) or (lovescore > 90):
+    print(f"Your compatibility score is {lovescore}, you go together like coke and mentos.")
+elif (lovescore >= 40) and (lovescore <= 50):
+    print(f"Your compatibility score is {lovescore}, you are alright together.")
+else:
+    print(f"Your compatibility score is {lovescore}.")
